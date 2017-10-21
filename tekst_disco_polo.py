9a58eab52539de8504c0db29ae25af465077630e
MagdaSzultk <magdalenaszultk@gmail.com>
2017-10-21 11:13:57 +0200

    na szybko - coś tam zrobiła - wrzuca do jsona :)

--- tekst_disco_polo.py
+++ tekst_disco_polo.py
@@ -1,3 +1,4 @@
+import random
 class TekstDiscoPolo:
     def __init__(self, wersy):
         self.wersy = wersy
@@ -7,11 +8,25 @@ class TekstDiscoPolo:
         linijki_tekstu = plik.readlines()
         self.wersy = []
         for linijka in linijki_tekstu:
-            self.wersy.append(linijka)
+            self.wersy.append(linijka.rstrip())
         for lista in self.wersy:
-            if lista ==[]:
+            if lista ==[] and lista=="":
                 self.wersy.remove([])
+                self.wersy.remove("")
         return self.wersy
+
+    def wrzucanie_do_jsona(self, wersy):
+        json = {
+            'zwrotki':[],
+            'refren':[]
+        }
+
+        for i in range(0,4):
+            json['zwrotki'].append(random.choice(wersy))
+        for i in range(0,4):
+            json['refren'].append(random.choice(wersy))
+        return json
+
 
     def generator_tekstu(self, wersy):
         str = ""
@@ -22,4 +37,5 @@ class TekstDiscoPolo:
 instancja = TekstDiscoPolo([])
 wersy = instancja.wczytaj_tekst('tekst_disco.txt')
 # print(wersy)
-print(instancja.generator_tekstu(wersy))
+# print(instancja.generator_tekstu(wersy))
+# print(instancja.wrzucanie_do_jsona(wersy))
