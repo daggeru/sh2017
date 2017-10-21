3428c74b4d4e272d837884cb29be85319bbb2290
daggeru <eskalator666>
2017-10-21 09:28:11 +0200

    sh2017 in the morning

--- index.py
+++ index.py
@@ -15,13 +15,13 @@ class GeneratorPiosenki:
     def on_get(self, req, resp):
         tekst = {
             'zwrotki': [
-                'pierwsza linijka#1\ndruga linijka#1\ntrzecia linijka#1\nczwarta linijka#1',
-                'pierwsza linijka#2\ndruga linijka#2\ntrzecia linijka#2\nczwarta linijka#2',
-                'pierwsza linijka#3\ndruga linijka#3\ntrzecia linijka#3\nczwarta linijka#3'
+                ['pierwsza linijka', 'druga linijka', 'trzecia linijka', 'czwarta linijka'],
+                ['pierwsza linijka', 'druga linijka', 'trzecia linijka', 'czwarta linijka'],
+                ['pierwsza linijka', 'druga linijka', 'trzecia linijka', 'czwarta linijka'],
             ],
             'refreny': [
-                'pierwsza linijkaREF#1\ndruga linijkaREF#1\ntrzecia linijkaREF#1\nczwarta linijkaREF#1',
-                'pierwsza linijkaREF#2\ndruga linijkaREF#2\ntrzecia linijkaREF#2\nczwarta linijkaREF#2'
+                ['pierwsza linijka', 'druga linijka', 'trzecia linijka', 'czwarta linijka'],
+                ['pierwsza linijka', 'druga linijka', 'trzecia linijka', 'czwarta linijka'],
             ]
         }
 
